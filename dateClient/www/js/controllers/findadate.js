35d2f03da122a91db869eda7676dac246a9b31bb
Alexander Liotta <aliotta343@gmail.com>
2015-10-29 17:29:16 -0700

    Update findadate.js

--- dateClient/www/js/controllers/findadate.js
+++ dateClient/www/js/controllers/findadate.js
@@ -96,8 +96,8 @@ angular.module('dateworthy.findadate', [])
     }
   };
 
-  // This example adds a search box to a map, using the Google Place Autocomplete
-  // feature. People can enter geographical searches. The search box will return a
+  
+  // Initializes map and allows people to enter geographical searches. The search box will return a
   // pick list containing a mix of places and predicted search terms.
 
   $scope.initMap = function() {
