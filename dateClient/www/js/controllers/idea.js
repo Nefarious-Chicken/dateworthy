bad0f241df35c5ef52b8197da7c0299c419ff60a
jingpei <jingjpei@gmail.com>
2015-11-03 19:51:27 -0800

    (fix) Fixed bugs with placeholder image on idea and favorite page

--- dateClient/www/js/controllers/idea.js
+++ dateClient/www/js/controllers/idea.js
@@ -8,6 +8,7 @@ angular.module('dateworthy.idea', ['ngOpenFB', 'ngCordova'])
   $scope.ideas = {}; 
 
   $scope.initMap = function(latitude, longitude, name){
+    console.log($scope.idea);
     console.log("Initiating Map...", latitude, longitude);
     var myLatlng = new google.maps.LatLng(latitude, longitude);
     var mapOptions = {
@@ -47,9 +48,11 @@ angular.module('dateworthy.idea', ['ngOpenFB', 'ngCordova'])
         console.log($scope.ideas);
         $scope.currentIdea = Number($stateParams.ideaId);
         $scope.imgWidth = window.innerWidth + 'px';
-        console.log($scope.idea.imgUrl); 
         console.log("innerwidth is", $scope.imgWidth);
         $scope.idea = $scope.ideas[$scope.currentIdea];
+        if(!$scope.idea.imgUrl){
+          $scope.idea.imgUrl = "./img/placeholder.jpg";
+        }
         $scope.idea.index = $scope.currentIdea;
         $scope.idea.last = false;
         $scope.idea.mapInit = false;
